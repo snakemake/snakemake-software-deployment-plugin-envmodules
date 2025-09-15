5ba379a7852ef1062c8e57b85a72b6dd86ee9ce7
Johannes Koester <johannes.koester@uni-due.de>
2025-09-15 16:04:28 +0200

    fix

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,18 +1,18 @@
 import os
 from typing import Optional, Type
-from snakemake_interface_software_deployment_plugins import EnvBase
+from snakemake_interface_software_deployment_plugins import EnvBase, EnvSpecBase
 from snakemake_interface_software_deployment_plugins.tests import (
     TestSoftwareDeploymentBase,
 )
 from snakemake_interface_software_deployment_plugins.settings import (
     SoftwareDeploymentSettingsBase,
 )
-from snakemake_software_deployment_plugin_envmodules import Env, EnvSpec, EnvSpecBase
+from snakemake_software_deployment_plugin_envmodules import Env, EnvSpec
 
 os.environ["MODULEPATH"] = "tests/modules"
 
 
-class TestSoftwareDeployment(TestSoftwareDeploymentBase):
+class Test(TestSoftwareDeploymentBase):
     __test__ = True  # activate automatic testing
 
     def get_software_deployment_provider_settings(
@@ -29,6 +29,17 @@ class TestSoftwareDeployment(TestSoftwareDeploymentBase):
         # this method should return an existing environment spec that can be used for
         # testing.
         return EnvSpec("somecmd")
+
+    def get_settings_cls(self) -> Optional[Type[SoftwareDeploymentSettingsBase]]:
+        # Return the settings class that should be used for this plugin.
+        return None
+
+    def get_settings(
+        self,
+    ) -> Optional[SoftwareDeploymentSettingsBase]:
+        # If your plugin has settings, return a valid settings object here.
+        # Otherwise, return None.
+        return None
 
     def get_test_cmd(self) -> str:
         # Return a command that should be executable without error in the environment
