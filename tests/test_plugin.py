6158ea5ba7e02abfff2b65d378ece0a98982236d
Johannes Koester <johannes.koester@uni-due.de>
2025-09-15 16:42:00 +0200

    dbg

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -14,6 +14,7 @@ os.environ["MODULEPATH"] = "tests/modules"
 
 class Test(TestSoftwareDeploymentBase):
     __test__ = True  # activate automatic testing
+    shell_executable = "bash"
 
     def get_software_deployment_provider_settings(
         self,
